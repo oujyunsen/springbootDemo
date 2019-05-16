ba61bdcd6d5c0f31f66d6bcffbc70c8da3c2db9a
Wang Shun Xuan <wangsxdl@cn.ibm.com>
2019-05-16 15:09:16 +0800

    springbootdemo

---
+++ demo01/src/main/java/com/sample/springboot/demo01/controller/Demo01Controller.java
@@ -0,0 +1,15 @@
+package com.sample.springboot.demo01.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Demo01Controller {
+
+  @RequestMapping(name="/version", method=RequestMethod.GET)
+  public String getVersion() {
+    return "v01";
+  }
+
+}
